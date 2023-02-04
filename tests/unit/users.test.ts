3e40f6ee495fcfb2ba47ba4a5ece0f3f2ced4dd3
barbararech <rech.brb@gmail.com>
2023-02-04 17:51:15 -0300

    fix: fix linters

--- tests/unit/users.test.ts
+++ tests/unit/users.test.ts
@@ -12,9 +12,9 @@ describe('Test POST /sign-up', () => {
 
   it('Should return 200 if  create user correctly', async () => {
     const user = await createUserFactory();
-
+    /* eslint-disable-next-line */
     jest.spyOn(userRepository, 'findUserByEmail').mockImplementationOnce((): any => {});
-
+    /* eslint-disable-next-line */
     jest.spyOn(userRepository, 'insertNewUser').mockImplementationOnce((): any => {});
 
     await signUp(user.email, user.password, user.username);
@@ -24,6 +24,7 @@ describe('Test POST /sign-up', () => {
   it('Should return 409 if registered a user that already exists', async () => {
     const user = await createUserFactory();
 
+    /* eslint-disable-next-line */
     jest.spyOn(userRepository, 'findUserByEmail').mockImplementationOnce((): any => {
       return {
         email: user.email,
